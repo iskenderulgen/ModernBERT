b870f2a4e23a79d25cb28fb89455d3b11ab5f8e0
iskenderulgen <iskenderogul@gmail.com>
2025-03-30 19:56:23 +0300

    update: use same code given by the github repo

--- src/bert_layers/normalization.py
+++ src/bert_layers/normalization.py
@@ -89,16 +89,27 @@ else:
     TritonLayerNorm = None
 
 
-class DyT(nn.Module):
-    def __init__(self, num_features, alpha_init_value=0.5):
+class DynamicTanh(nn.Module):
+    def __init__(self, normalized_shape, channels_last=True, alpha_init_value=0.5):
         super().__init__()
+        self.normalized_shape = normalized_shape
+        self.alpha_init_value = alpha_init_value
+        self.channels_last = channels_last
+
         self.alpha = nn.Parameter(torch.ones(1) * alpha_init_value)
-        self.weight = nn.Parameter(torch.ones(num_features))
-        self.bias = nn.Parameter(torch.zeros(num_features))
-    
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+
     def forward(self, x):
         x = torch.tanh(self.alpha * x)
-        return x * self.weight + self.bias
+        if self.channels_last:
+            x = x * self.weight + self.bias
+        else:
+            x = x * self.weight[:, None, None] + self.bias[:, None, None]
+        return x
+
+    def extra_repr(self):
+        return f"normalized_shape={self.normalized_shape}, alpha_init_value={self.alpha_init_value}, channels_last={self.channels_last}"
 
 
 
@@ -107,7 +118,7 @@ NORM2CLS = {
     "triton_layernorm": TritonLayerNorm if TritonLayerNorm is not None else nn.LayerNorm,
     "rmsnorm": RMSNorm,
     "triton_rmsnorm": TritonRMSNorm if TritonRMSNorm is not None else RMSNorm,
-    "dynamic_tanh": DyT # https://jiachenzhu.github.io/DyT/
+    "dynamic_tanh": DynamicTanh # https://jiachenzhu.github.io/DyT/
 }
 
 
